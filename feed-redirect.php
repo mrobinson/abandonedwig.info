2868c31916ac9f950605c2ccdccd2f82f0852fe2
Martin Robinson <mrobinson@igalia.com>
2015-04-14 12:40:10 -0700

    Try harder to avoid errors in feed redirect script

--- feed-redirect.php
+++ feed-redirect.php
@@ -1,7 +1,7 @@
 <?php
 header("HTTP/1.1 301 Moved Permanently");
 
-if ($_REQUEST['alt'] == 'rss') {
+if (isset($_REQUEST['alt']) && $_REQUEST['alt'] == 'rss') {
     header("Location: http://abandonedwig.info/blog/rss.xml");
 } else {
     header("Location: http://abandonedwig.info/blog/atom.xml");
