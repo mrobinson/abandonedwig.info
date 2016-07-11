f4f5c951b80cb9e53c1a194104865747de77aeaa
Martin Robinson <mrobinson@igalia.com>
2016-07-11 09:13:11 -0700

    Support newer jekyll interfaces
    Turns out things change in the space of three years.

--- jekyll/_plugins/extend_post.rb
+++ jekyll/_plugins/extend_post.rb
@@ -1,11 +1,13 @@
+require 'pathname'
+
 module Jekyll
 
-    class Post
+    class Document
       alias_method :original_to_liquid, :to_liquid
 
       def to_liquid
         data = original_to_liquid
-        data['filename'] = @name
+        data['filename'] = Pathname.new(@path).basename.to_s
         return data
       end
     end
