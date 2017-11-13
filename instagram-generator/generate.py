9ae3c7653dc9e7025e0b1718a3fb03fd866a6fa6
Martin Robinson <mrobinson@igalia.com>
2017-11-13 17:02:16 +0100

    Fix some issues with the instagram scraper

--- instagram-generator/generate.py
+++ instagram-generator/generate.py
@@ -29,20 +29,35 @@ def run_command(command_string):
         sys.exit()
 
 
+def get_caption(entry):
+    try:
+        return entry['caption']['text']
+    except KeyError:
+        pass
+    try:
+        return entry['edge_media_to_caption']['edges'][0]['node']['text']
+    except KeyError:
+        return "Untitled photo"
+
+
+def get_timestamp(entry):
+    try:
+        return float(entry['created_time'])
+    except KeyError:
+        return float(entry['taken_at_timestamp'])
+
+
 with open('{}/{}.json'.format(DATA_DIR, USER_NAME), 'r') as data_file:
     data = json.load(data_file)
 
 run_command("mkdir -p " + POSTS_PATH)
 
 for entry in data:
-    timestamp = float(entry['created_time'])
+    timestamp = get_timestamp(entry)
     date = datetime.datetime.utcfromtimestamp(timestamp)
     date_string = date.strftime("%Y-%m-%d")
 
-    if 'caption' in entry and entry['caption'] and 'text' in entry['caption']:
-        caption = entry['caption']['text']
-    else:
-        caption = "Untitled photo"
+    caption = get_caption(entry)
 
     images = [get_filename(url) for url in entry['urls']]
     print(str(entry['urls']))
@@ -78,9 +93,12 @@ for entry in data:
             post_file.write('<div class="photo">\n')
             if image.find(".mp4") != -1:
                 post_file.write("  <video controls>\n")
-                post_file.write('    <source src="{{ page.images_dir }}/' + image + '" type="video/mp4">\n')
-                post_file.write('    Your browser does not support the video tag.\n')
+                post_file.write('    <source src="{{ page.images_dir }}/' +
+                                image + '" type="video/mp4">\n')
+                post_file.write(
+                    '    Your browser does not support the video tag.\n')
                 post_file.write('  </video>\n')
             else:
-                post_file.write('  <img src="{{ page.images_dir }}/' + image + '"/>\n')
+                post_file.write('  <img src="{{ page.images_dir }}/' +
+                                image + '"/>\n')
             post_file.write('</div>\n')
