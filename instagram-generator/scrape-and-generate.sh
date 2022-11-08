1a0fdeea73439cc796f011081072796a6c664efe
Martin Robinson <mrobinson@igalia.com>
2022-11-08 11:51:24 +0100

    Rework layout and remove images
    The new site layout is a standard jekyll site, which should make
    migration easier. All mirrored instagram images are also removed.

--- instagram-generator/scrape-and-generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-USER='abandonedwhig'
-pipenv run instagram-scraper --login-user $USER --login-pass ` pass show instagram.com | head -1` --latest --media-metadata $USER
-pipenv run ./generate.py $USER
